913004216373de91da1a9482be1fd24c3c88c429
Zheyu Yan <zheyuyan@zheyus-air.myfiosgateway.com>
2022-05-05 03:36:03 -0400

    update

--- app.js
+++ app.js
@@ -440,7 +440,7 @@ app.use(function(err, req, res, next) {
 //  Starting up the server!
 // *********************************************************** //
 //Here we set the port to use between 1024 and 65535  (2^16-1)
-const port = "5000";
+const port = process.env.PORT || "5000";
 app.set("port", port);
 
 // and now we startup the server listening on that port
